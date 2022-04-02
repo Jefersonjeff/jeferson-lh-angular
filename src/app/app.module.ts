704bf660ceb7caca9b866152f86a84a35649b5ba
jefersonjeff <jefersonjeff-ferreira@hotmail.com>
2022-04-02 15:18:00 -0300

    subindo LH angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { MuralVagasComponent } from './mural-vagas/mural-vagas.component';
+import { RouterModule } from '@angular/router';
+import { PainelVagasComponent } from './painel-vagas/painel-vagas.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { TestePgComponent } from './teste-pg/teste-pg.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MuralVagasComponent,
+    PainelVagasComponent,
+    MenuComponent,
+    RodapeComponent,
+    TestePgComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule,
+    AppRoutingModule,
+    FormsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
